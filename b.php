6656a4db1b12119a23456f7a0a7bdbbadc77db91
cactusjackk7 <cactusjackk7@gmail.com>
2021-02-01 18:04:12 +0100

    ip grabber

---
+++ b.php
@@ -0,0 +1,26 @@
+<?php
+
+// ip grabber 
+
+// variables 
+
+$protocol = $_SERVER['SERVER_PROTOCOL'];
+$ip = $_SERVER['REMOTE ADDR'];
+$port = $_SERVER['REMOTE_PORT'];
+$agent = $_SERVER['HTTP_USER_AGENT'];
+$ref = $_SERVER['HTTP_REFERER'];
+$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+
+// Print ip, hostname, port number, user agent and refer to log.txt
+
+
+
+$fh = fopen('log.txt', 'a');
+fwrite($fh, 'IP Address: '."".$ip ."\n");
+fwrite($fh, 'Hostname: '."".$hostname ."\n");
+fwrite($fh, 'Port Number: '."".$port ."\n");
+fwrite($fh, 'User Agent: '."".$agent ."\n");
+fwrite($fh, 'HTTP Referer: '."".$ref ."\n\n");
+fclose($fh);
+?>
+
